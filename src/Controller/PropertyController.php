e0323792ba45d42e76d4fb4c3f522490835be43e
Vivien Frébourg <vivien.frebourg@outlook.com>
2019-11-13 15:51:57 +0100

    Premier commit des fichiers du projet Symfony.

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PropertyController extends AbstractController
+{
+    /**
+     * @Route("/biens", name="property.index")
+     */
+    public function index()
+    {
+        return $this->render('property/index.html.twig', [
+            'controller_name' => 'PropertyController',
+        ]);
+    }
+}
