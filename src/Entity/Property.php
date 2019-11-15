d1e63376add8ddb56a20b2db5a3cc89ede48452c
Vivien Frébourg <vivien.frebourg@outlook.com>
2019-11-15 17:29:51 +0100

    Vérification des données (Erreurs saisies, type données attendues) via Assert

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -4,9 +4,12 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PropertyRepository")
+ * @UniqueEntity("title")
  */
 class Property
 {
@@ -24,6 +27,7 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="5", max="255")
      */
     private $title;
 
@@ -34,6 +38,7 @@ class Property
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\Range(min="10", max="400")
      */
     private $surface;
 
@@ -74,6 +79,7 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex("/^[0-9]{5}$/")
      */
     private $postal_code;
 
