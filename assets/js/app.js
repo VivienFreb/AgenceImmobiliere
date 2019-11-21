040e2e21458dd490b8db2e4a4c4ce7f8a055ea43
Vivien Frébourg <vivien.frebourg@outlook.com>
2019-11-21 17:50:59 +0100

    Ajout Symfony Encore: Modification JS (Select2) et Ajout Babel

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+var $ = require('jquery');
+
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
+require('select2');
+
+$('select').select2()
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// const $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
